e3fd54ce4ed2132e27e81121eb7a6c679f38c7d3
itrvc <itrvculanag@outlook.com>
2022-04-20 23:50:43 +0800

    config: run hangaroo game

--- src/tech/stargeneration/main/Main.java
+++ src/tech/stargeneration/main/Main.java
@@ -1,11 +1,9 @@
 package tech.stargeneration.main;
 
-import tech.stargeneration.word.Word;
-
 public class Main {
 
     public static void main(String[] args) {
-        Word word = new Word("Hello");
-        System.out.println(word.getFormattedWord());
+        Hangaroo hangaroo = new Hangaroo();
+        hangaroo.startGame();
     }
 }
