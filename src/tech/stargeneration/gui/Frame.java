27c1d606fabd55210255369ff8f280568d5c8b3c
itrvc <itrvculanag@outlook.com>
2022-04-21 20:36:54 +0800

    config: init frame and add screen

--- src/tech/stargeneration/gui/Frame.java
+++ src/tech/stargeneration/gui/Frame.java
@@ -1,4 +1,22 @@
 package tech.stargeneration.gui;
 
-public class Frame {
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+
+    public Frame() {
+
+        Screen screen = new Screen();
+
+        setTitle("Hangaroo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        add(screen);
+
+        pack();
+        setVisible(true);
+    }
 }
