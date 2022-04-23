b7fec9a9b15bd8852ba4e7724ba701a34f5d0ba7
itrvc <itrvculanag@outlook.com>
2022-04-23 13:20:59 +0800

    refactor: remove cli option for gui

--- src/tech/stargeneration/main/Hangaroo.java
+++ src/tech/stargeneration/main/Hangaroo.java
@@ -1,35 +1,17 @@
 package tech.stargeneration.main;
 
-import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Hangaroo {
 
-    private final ArrayList<String> words;
     private final Random random;
-    private String word;
-    private int maxTries;
+    private final String word;
+    private final int maxTries;
 
     public Hangaroo() {
-        words = new ArrayList<>();
+        ArrayList<String> words = new ArrayList<>();
         random = new Random();
-
-//        try {
-//            file = new File(".\\res\\words.txt");
-//
-//            Scanner scanner = new Scanner(file);
-//
-//            while (scanner.hasNextLine()) {
-//                String word = scanner.nextLine();
-//                words.add(word);
-//            }
-//
-//            scanner.close();
-//        } catch (FileNotFoundException e) {
-//            System.out.printf("File %s does not exist.", file.getName());
-//        }
 
         words.add("bling");
         words.add("jumpy");
@@ -64,54 +46,6 @@ public class Hangaroo {
         int randomWord = random.nextInt(words.size());
         word = words.get(random.nextInt(randomWord));
         maxTries = 3;
-    }
-
-    // Use start game if you want to play in the CLI.
-    public void startGame() {
-        Scanner input = new Scanner(System.in);
-        String randomWord = words.get(random.nextInt(words.size()));
-        String formattedWord = formatWord(randomWord);
-        String userGuess;
-
-        int tryCounter = 0;
-
-        System.out.println("Word to guess: "
-                + formattedWord
-                .replace("", " ")
-                .toUpperCase());
-
-        while (tryCounter != maxTries) {
-            System.out.print("\nEnter a letter to complete the word: ");
-            userGuess = input.nextLine().toLowerCase();
-
-            if (randomWord.contains(userGuess)) {
-                formattedWord = deconstructFormattedWord(
-                        randomWord,
-                        formattedWord,
-                        userGuess
-                );
-
-                System.out.println("Word to guess: "
-                        + formattedWord
-                        .replace("", " ")
-                        .toUpperCase());
-
-                tryCounter += 1;
-            } else {
-                System.out.printf("The letter %s is not in the word. Try again\n", userGuess);
-            }
-
-        }
-
-        System.out.printf("\nYou have already tried to guess the word in %d attempts.", maxTries);
-        System.out.print("\nGuess the word now: ");
-        userGuess = input.nextLine();
-
-        String message = (randomWord.equals(userGuess))
-                ? "\nCongratulations! You guessed the word."
-                : "\nBetter luck next time! The word was %s".formatted(randomWord);
-
-        System.out.println(message);
     }
 
     public String formatWord(String word) {
